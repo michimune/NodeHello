436efdda43bf201f358a2bd88962e78230a1b632
Nazim Lala <naziml@gmail.com>
2016-09-08 18:38:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from express!');
+  res.send('PM2 are your awake?');
 });
 
 app.listen(process.env.PORT, function () {
