e670ac96c3c720c0d22a680709639f8a041bea1d
Nazim Lala <naziml@gmail.com>
2016-09-12 08:17:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello there!');
+  res.send('Linux is awesome!');
 });
 
 app.listen(process.env.PORT, function () {
