21ed7eccceefe61744d889d81e4cc9b3ae319340
michimune <michimune@outlook.com>
2017-05-30 00:21:10 -0700

    Update text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Linux is awesome! Made a change');
+  res.send('Open source is awesome! Made a change');
 });
 
 app.listen(process.env.PORT, function () {
